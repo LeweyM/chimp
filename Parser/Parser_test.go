916f2076d543143a8f6c51a73ec4f4e9913cc5df
Lewis Bonathan Metcalf <lewis.metcalf@thoughtworks.com>
2019-09-29 22:48:08 +0200

    restructured parse let statement tests

--- Parser/Parser_test.go
+++ Parser/Parser_test.go
@@ -8,91 +8,49 @@ import (
 )
 
 func TestParseLetStatement(t *testing.T) {
-	var tests = []struct {
-		input          string
-		expectedToken  Token.Token
-		expectedName   Ast.Expression
-		expectedValue  Ast.Expression
-		expectedErrors []string
-	}{
-		{
-			input:         "let five = 5;",
-			expectedToken: Token.Token{Type: Token.LET, Literal: "let"},
-			expectedName: Ast.IdentityExpression{
-				Token: Token.Token{Type: Token.IDENT, Literal: "five"},
-				Value: "five",
-			},
-			expectedValue: Ast.IntegerExpression{
-				Token: Token.Token{Type: Token.INT, Literal: "5"},
-				Value: 5,
-			},
-			expectedErrors: []string{},
-		}, {
-			input:         "let foo = 67",
-			expectedToken: Token.Token{Type: Token.LET, Literal: "let"},
-			expectedName: Ast.IdentityExpression{
-				Token: Token.Token{Type: Token.IDENT, Literal: "foo"},
-				Value: "foo",
-			},
-			expectedValue: Ast.IntegerExpression{
-				Token: Token.Token{Type: Token.INT, Literal: "67"},
-				Value: 67,
-			},
-			expectedErrors: []string{},
-		},
+	input := `
+		let foo = 67;
+		let five = 5;
+	`
+	literals := []string{"foo", "five"}
+	values := []int64{67, 5}
+
+	l := Lexer.New(input)
+	p := New(*l)
+
+	programme := p.ParseProgramme()
+	checkForErrors(p, t)
+
+	if len(programme.Statements) != 2 {
+		t.Fatalf("Expected 2 statements, got %d", len(programme.Statements))
 	}
 
-	for _, tt := range tests {
-		l := Lexer.New(tt.input)
-		p := New(*l)
-
-		programme := p.ParseProgramme()
-
-		if len(programme.Statements) != 1 {
-			t.Fatalf("wrong number of statements in programme; wanted 1, got %d", len(programme.Statements))
-		}
-
-		statement := programme.Statements[0]
+	for i, statement := range programme.Statements {
 
 		letStatement, ok := statement.(*Ast.LetStatement)
 		if !ok {
 			t.Fatal("Not of type LetStatement")
 		}
 
-		checkForErrors(p, t)
-
-		if tt.expectedToken.Type != letStatement.Token.Type {
-			t.Fatalf("Expected type of %s, got %s", tt.expectedToken.Type, letStatement.Token.Type)
+		if letStatement.Token.Type != Token.LET {
+			t.Fatalf("Expected type of LET, got %s", letStatement.Token.Type)
 		}
 
-		if tt.expectedToken.Literal != letStatement.Token.Literal {
-			t.Fatalf("Expected let literal of %s, got %s", tt.expectedToken.Literal, letStatement.TokenLiteral())
-		}
-
-		identityExpression, ok := tt.expectedName.(Ast.IdentityExpression)
-		if !ok {
-			t.Fatal("Identifier not of type IdentityExpression")
-		}
-
-		if identityExpression.Value != letStatement.Name.Value {
-			t.Fatalf("Expected identifier name of %s, got %s", tt.expectedName, letStatement.Name)
-		}
-
-		expectedIntegerExpression, ok := tt.expectedValue.(Ast.IntegerExpression)
-		if !ok {
-			t.Fatal("Expected value not of type IntegerExpression")
+		if letStatement.Name.Value != literals[i] {
+			t.Fatalf("Expected identifier name of %s, got %s", literals[i], letStatement.Name)
 		}
 
 		integerExpression, ok := letStatement.Value.(*Ast.IntegerExpression)
 		if !ok {
-			t.Fatal("Value not of type IntegerExpression")
+			t.Fatal("Expected value not of type IntegerExpression")
 		}
 
-		if expectedIntegerExpression.Value != integerExpression.Value {
-			t.Fatalf("Expected value of %s, got %s", tt.expectedValue, letStatement.Value)
+		if integerExpression.Value != values[i] {
+			t.Fatalf("Expected value of %d, got %d", values[i], letStatement.Value)
 		}
 
 	}
+
 }
 
 func TestParseReturnStatements(t *testing.T) {
@@ -100,7 +58,6 @@ func TestParseReturnStatements(t *testing.T) {
 		return 5;
 		return 500;
 	`
-
 	values := []int64{5, 500}
 
 	l := *Lexer.New(input)
