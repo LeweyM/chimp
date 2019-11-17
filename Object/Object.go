328e0fe32d2931b766e99ffe1a39e5de95b3f91a
Lewis Bonathan Metcalf <lewis.metcalf@thoughtworks.com>
2019-11-17 20:36:20 +0100

    add Object package and basic integer obj

---
+++ Object/Object.go
@@ -0,0 +1,19 @@
+package Object
+
+import "fmt"
+
+type ObjectType string
+
+const INTEGER_OBJ = "INTEGER"
+
+type Object interface {
+	Type() ObjectType
+	Inspect() string
+}
+
+type Integer struct {
+	value int64
+}
+
+func (i Integer) Type() ObjectType { return INTEGER_OBJ }
+func (i Integer) Inspect() string { return fmt.Sprintf("%d", i.value) }
